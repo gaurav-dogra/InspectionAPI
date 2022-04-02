1b2bd2ea89bb50ea9bf20428e2da7742200b9af3
Gaurav Dogra <gauru.dogra@gmail.com>
2022-04-02 21:08:27 +0100

    ASP.NET API with basic CRUD

---
+++ InspectionAPI/Migrations/20220402165209_InspectionType.InspectionName_ToString_FromInt.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InspectionAPI.Migrations
+{
+    public partial class InspectionTypeInspectionName_ToString_FromInt : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "InspectionName",
+                table: "InspectionTypes",
+                type: "nvarchar(20)",
+                maxLength: 20,
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldMaxLength: 20);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "InspectionName",
+                table: "InspectionTypes",
+                type: "int",
+                maxLength: 20,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(20)",
+                oldMaxLength: 20);
+        }
+    }
+}
